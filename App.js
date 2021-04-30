6a6dc5a926a58ce72c13939ba04abcb94545968d
maksym <kovmaxter@gmail.com>
2021-04-30 17:36:59 +0300

    Lesson-2+Lesson-2-skrig

--- App.js
+++ App.js
@@ -1,51 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
-import User from "./component/User";
+import {getUser, getUsers} from "./src/api/Api";
+import UserComponets from "./src/components/UsersComponent";
+import UsersComponent from "./src/components/UsersComponent";
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
+import UsersDetailsComponent from "./src/components/UsersDetailsComponent";
 
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+let StackNavigator=createStackNavigator();
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      {/*<Text style={styles.text}>Viva Maxter-You are programmer ))</Text>*/}
-      {/*<StatusBar style="auto" />*/}
-      <FlatList data={users}
-                renderItem={({item})=>{
-                    console.log(item);
-                    return <User item={item}/>;
-                }}
-                />
-    </View>
+      <NavigationContainer>
+<StackNavigator.Navigator>
+    <StackNavigator.Screen name={'UsersComponent'} component={UsersComponent}/>
+    <StackNavigator.Screen name={'UserDetails'} component={UsersDetailsComponent}/>
+
+
+</StackNavigator.Navigator>
+
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // text: {
-  //   fontSize: 40,
-  // }
-  //   box: {
-  //       height: 100,
-  //       width: '50%',
-  //       marginBottom: 10,
-  //   },
-  //   item: {
-  //       backgroundColor: 'tomato',
-  //   },
-});
+const styles = StyleSheet.create({});
