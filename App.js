2ac760f1b2ad7333030cf919e0357d82f037cfc6
maksym <kovmaxter@gmail.com>
2021-05-01 22:10:57 +0300

    Lesson-3-skrig

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {getUser, getUsers} from "./src/api/Api";
-import UserComponets from "./src/components/UsersComponent";
+
 import UsersComponent from "./src/components/UsersComponent";
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
@@ -12,12 +12,11 @@ export default function App() {
 
   return (
       <NavigationContainer>
-<StackNavigator.Navigator>
-    <StackNavigator.Screen name={'UsersComponent'} component={UsersComponent}/>
-    <StackNavigator.Screen name={'UserDetails'} component={UsersDetailsComponent}/>
+            <StackNavigator.Navigator>
+                <StackNavigator.Screen name={'UsersComponent'} component={UsersComponent}/>
+                <StackNavigator.Screen name={'UserDetails'} component={UsersDetailsComponent}/>
 
-
-</StackNavigator.Navigator>
+            </StackNavigator.Navigator>
 
       </NavigationContainer>
   );
