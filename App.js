04e7ea13302f9ce2c454794c0f1c466cce3e6870
maksym <kovmaxter@gmail.com>
2021-05-07 17:55:10 +0300

    Lesson-3-skrigaliv

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {getUser, getUsers} from "./src/api/Api";
-
 import UsersComponent from "./src/components/UsersComponent";
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
 import UsersDetailsComponent from "./src/components/UsersDetailsComponent";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import PostsComponent from "./src/components/PostsComponent";
+import PostDrawer from "./src/components/PostDrawer";
 
-let StackNavigator=createStackNavigator();
+let BottomTabNavigator= createBottomTabNavigator();
 export default function App() {
 
   return (
       <NavigationContainer>
-            <StackNavigator.Navigator>
-                <StackNavigator.Screen name={'UsersComponent'} component={UsersComponent}/>
-                <StackNavigator.Screen name={'UserDetails'} component={UsersDetailsComponent}/>
-
-            </StackNavigator.Navigator>
+            <BottomTabNavigator.Navigator tabBarOptions={{tabStyle: {justifyContent: 'center', alignItems: 'center'}}}>
+                <BottomTabNavigator.Screen name={'users'} component={UsersComponent}/>
+                <BottomTabNavigator.Screen name={'posts'} component={PostDrawer}/>
+            </BottomTabNavigator.Navigator>
 
       </NavigationContainer>
   );
