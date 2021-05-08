12e3d712ecce70f3d4520238a363e6a7c7edb424
maksym <kovmaxter@gmail.com>
2021-05-08 16:38:58 +0300

    Lesson-4-skrigaliv

--- App.js
+++ App.js
@@ -8,15 +8,16 @@ import UsersDetailsComponent from "./src/components/UsersDetailsComponent";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import PostsComponent from "./src/components/PostsComponent";
 import PostDrawer from "./src/components/PostDrawer";
+import CameraComponent from "./src/components/CameraComponent";
 
 let BottomTabNavigator= createBottomTabNavigator();
 export default function App() {
-
   return (
       <NavigationContainer>
             <BottomTabNavigator.Navigator tabBarOptions={{tabStyle: {justifyContent: 'center', alignItems: 'center'}}}>
                 <BottomTabNavigator.Screen name={'users'} component={UsersComponent}/>
                 <BottomTabNavigator.Screen name={'posts'} component={PostDrawer}/>
+                <BottomTabNavigator.Screen name={'camera'} component={CameraComponent} options={{unmountOnBlur: true}}/>
             </BottomTabNavigator.Navigator>
 
       </NavigationContainer>
